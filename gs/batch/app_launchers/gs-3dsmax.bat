093e1703cfaee6393ba4d6d28e87d6f99c8fcadf
Adam Burke <aburke@gentscholar.com>
2018-03-19 15:58:10 -0400

    merged with nuketools and a little cleanup

---
+++ gs/batch/app_launchers/gs-3dsmax.bat
@@ -0,0 +1,7 @@
+set "ST_3DSMAX=2016"
+set "ST_3DSMAX_DIR=C:/Program Files/Autodesk/3dsMax/%ST_3DSMAX%.0.0"
+set "GSBRANCH=//scholar/pipeline/base"
+set "LM_LICENSE_FILE=27001@laxlic01"
+set "AUTODESK_ADLM_THINCLIENT_ENV=//scholar/apps/Autodesk/adlm/AdlmThinClientCustomEnv.xml"
+
+"C:\Program Files\Autodesk\3dsMax\2016.0.0\3dsmax.exe"
